e1fd01cf45e30495279e255b565afee5886e1958
wvlasenko <44614847+wvlasenko@users.noreply.github.com>
2021-02-02 22:00:12 +0200

    Clear todo, fix filter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-// import { useState, useEffect } from 'react';
-// import { v4 as uuid4 } from 'uuid';
 import { connect } from 'react-redux';
 import * as actions from './redux/contacts-actions';
 import ContactForm from './componnents/ContactForm/ContactForm';
@@ -7,56 +5,8 @@ import ContactsList from './componnents/ContactList/ContactList';
 import Filter from './componnents/Filter/Filter';
 import AppStl from './App.module.css';
 
-// const defaultContacts = [
-//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-// ];
-
 function App({ contacts }) {
   console.log(actions);
-  // const [contacts, setContacts] = useState(() => {
-  //   return (
-  //     JSON.parse(window.localStorage.getItem('contacts')) ?? defaultContacts
-  //   );
-  // });
-  //
-  // useEffect(() => {
-  //   window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-
-  // const addContact = (name, number) => {
-  //   if (contacts.some(contact => contact.name === name)) {
-  //     alert(`${name} is already in contacts`);
-  //     return;
-  //   }
-  //   setContacts(contacts => {
-  //     const contact = {
-  //       id: uuid4(),
-  //       name,
-  //       number,
-  //     };
-  //     return [...contacts, contact];
-  //   });
-  // };
-
-  // const removeContact = contactId => {
-  //   setContacts(contacts.filter(({ id }) => id !== contactId));
-  // };
-  //
-  // const [filter, setFilter] = useState('');
-
-  // const onInputFilter = e => {
-  //   const { value } = e.target;
-  //   setFilter(value);
-  // };
-
-  // const getVisibleContacts = (contacts, filter) => {
-  //   return contacts.filter(contact =>
-  //     contact.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase()),
-  //   );
-  // };
 
   return (
     <div className={AppStl.container}>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,10 @@
 import { combineReducers } from 'redux';
 import contactReducer from './contacts-reducer';
 import { configureStore } from '@reduxjs/toolkit';
-// import { composeWithDevTools } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers({
   contacts: contactReducer,
 });
-
-// const store = createStore(rootReducer, composeWithDevTools());
 
 const store = configureStore({
   reducer: rootReducer,
